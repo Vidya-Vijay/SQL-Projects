03451582fa0d504456b160a45e9fedec51b22906
Vidya Vijay <83777787+Vidya-Vijay@users.noreply.github.com>
2025-08-08 23:20:29 -0700

    Update queries.sql

--- Chemical_Plant_DB/queries.sql
+++ Chemical_Plant_DB/queries.sql
@@ -1,3 +1,48 @@
+--Data Definition Language (DDL)
+--Database: productionplant
+--The DDL for all the tables in the productionplant database are as follows:
+
+--Raw_Material 
+CREATE TABLE `productionplant`.`raw_material` (`RawMaterial_ID` VARCHAR(4) NOT NULL , `RawMaterial_Name` VARCHAR(15) NOT NULL , 
+  `RawMaterial_Price` DECIMAL(4,2) NOT NULL , `Supplier_ID` VARCHAR(3) NOT NULL , PRIMARY KEY (`RawMaterial_ID`)) ENGINE = InnoDB; 
+
+--Supplier 
+CREATE TABLE `productionplant`.`supplier` (`Supplier_ID` VARCHAR(3) NOT NULL , `Supplier_Name` VARCHAR(23) NOT NULL , `Supplier_Address` VARCHAR(27) NOT NULL , 
+  `Supplier_Country` VARCHAR(25) NOT NULL , `Supplier_Contact` VARCHAR(12) NOT NULL , PRIMARY KEY (`Supplier_ID`)) ENGINE = InnoDB;
+
+--Product_Material
+CREATE TABLE `productionplant`.`product_material` (`RawMaterial_ID` VARCHAR(15) NOT NULL , `Prod_ID` VARCHAR(15) NOT NULL , 
+  `QtyUsed` INT(25) NOT NULL ) ENGINE = InnoDB;
+
+--Employee
+CREATE TABLE `productionplant`.`employee` (`Emp_ID` INT(3) NOT NULL AUTO_INCREMENT , `Emp_Name` VARCHAR(13) NOT NULL , `Emp_Designation` VARCHAR(20) NOT NULL , 
+  `Emp_Email` VARCHAR(25) NOT NULL , `Emp_Contact` VARCHAR(12) NOT NULL , PRIMARY KEY (`Emp_ID`)) ENGINE = InnoDB;
+
+--Production
+CREATE TABLE `productionplant`.`production` (`Prod_ID` VARCHAR(3) NOT NULL , `Prod_Plant` VARCHAR(7) NOT NULL , `Prod_Shift` VARCHAR(9) NOT NULL , 
+  `Prod_Batch` VARCHAR(15) NOT NULL , `Product_ID` VARCHAR(4) NOT NULL , `Prod_Qty` INT(10) NOT NULL , `Emp_ID` INT(3) NOT NULL , 
+  PRIMARY KEY (`Prod_ID`)) ENGINE = InnoDB; 
+
+--Product
+CREATE TABLE `productionplant`.`product` (`Product_ID` VARCHAR(4) NOT NULL , `Product_Name` INT(20) NOT NULL , `Product_Qty` INT(3) NOT NULL , 
+  PRIMARY KEY (`Product_ID`)) ENGINE = InnoDB;
+
+--Orders
+CREATE TABLE `productionplant`.`orders` (`Order_ID` INT(4) NOT NULL AUTO_INCREMENT , `Order_Date` VARCHAR(10) NOT NULL , 
+  `Order_Status` VARCHAR(10) NOT NULL , `Cust_ID` VARCHAR(3) NOT NULL , PRIMARY KEY (`Order_ID`)) ENGINE = InnoDB; 
+
+--Order_Details
+CREATE TABLE `productionplant`.`order_details` (`Order_ID` INT(4) NOT NULL, `Product_ID` VARCHAR(10) NOT NULL , `OrderQty` INT(10) NOT NULL , 
+  `OrderPrice` INT(10) NOT NULL) ENGINE = InnoDB;
+
+--Payment
+CREATE TABLE `productionplant`.`payment` (`Payment_ID` INT(4) NOT NULL AUTO_INCREMENT , `Payment_Amt` DECIMAL(10,2) NOT NULL , 
+  `Payment_DueDate` VARCHAR(10) NOT NULL , `Payment_Status` VARCHAR(3) NOT NULL , `Order_ID` INT(4) NOT NULL , PRIMARY KEY (`Payment_ID`)) ENGINE = InnoDB; 
+
+--Customer
+CREATE TABLE `productionplant`.`customer` (`Cust_ID` VARCHAR(3) NOT NULL ,`Cust_Name` VARCHAR(18) NOT NULL , `Cust_Address` VARCHAR(25) NOT NULL , 
+  `Cust_Country` VARCHAR(15) NOT NULL , `Cust_Email` VARCHAR(22) NOT NULL ,`Cust_Contact` VARCHAR(12) NOT NULL , PRIMARY KEY (`Cust_ID `)) ENGINE = InnoDB; 
+
 -- Some SQL Queries and Results
 -- 1. List all production batches along with the associated raw materials:
 
@@ -37,3 +82,4 @@ FROM product JOIN production ON production.Product_ID = product.Product_ID
 OIN product_material ON product_material.Prod_ID = production.Prod_ID 
 JOIN raw_material ON product_material.RawMaterial_ID = raw_material.RawMaterial_ID 
 WHERE raw_material.RawMaterial_Price = (SELECT MAX(raw_material.RawMaterial_Price) FROM raw_material);
+
